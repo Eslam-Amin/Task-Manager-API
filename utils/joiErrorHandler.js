0536e7357cf1d2447b4dd17019668474596e4210
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-26 22:59:26 +0200

    converted JoiErrorHandler to a class with a static validate method

--- utils/joiErrorHandler.js
+++ utils/joiErrorHandler.js
@@ -1,13 +1,17 @@
+// joiErrorHandler.js
 const ApiError = require("../utils/ApiError");
 
-function joiErrorHandler(schema, data) {
-  const { error } = schema.validate(data);
-  if (!error) return;
-  let message = error.details[0].message.replace(/"/g, "").split(" ");
+class JoiErrorHandler {
+  static validate(schema, data) {
+    const { error } = schema.validate(data);
+    if (!error) return;
 
-  message = message.join(" ").split("_").join(" ");
-  message = `${message.charAt(0).toUpperCase()}${message.slice(1)}`;
-  throw new ApiError(message, 400);
+    let message = error.details[0].message.replace(/"/g, "").split(" ");
+    message = message.join(" ").split("_").join(" ");
+    message = `${message.charAt(0).toUpperCase()}${message.slice(1)}`;
+
+    throw new ApiError(message, 400);
+  }
 }
 
-module.exports = joiErrorHandler;
+module.exports = JoiErrorHandler;

b974a1f08f00b75d4b82fddd009226de46fb27ca
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-26 22:49:21 +0200

    added task priority

--- utils/constants.js
+++ utils/constants.js
@@ -3,3 +3,4 @@ exports.GENDER_LIST_EN = ["Male", "Female"];
 exports.USER = "user";
 
 exports.TASK_STATUS = ["pending", "in-progress", "completed"];
+exports.TASK_PROIORITY = ["low", "medium", "high"];

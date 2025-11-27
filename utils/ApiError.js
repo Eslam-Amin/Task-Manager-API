84c28bd65f65915985922dabe65bc9f3b64337a6
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-27 15:15:16 +0200

    Added static methods to be used with the class

--- utils/ApiError.js
+++ utils/ApiError.js
@@ -6,6 +6,18 @@ class ApiError extends Error {
     this.status = `${statusCode}`.startsWith(4) ? "Failed" : "Error";
     this.success = false;
   }
+
+  static badRequest(message) {
+    return new ApiError(message, 400);
+  }
+
+  static notFound(message) {
+    return new ApiError(message, 404);
+  }
+
+  static internal(message) {
+    return new ApiError(message, 500);
+  }
 }
 
-module.exports = new ApiError();
+module.exports = ApiError;

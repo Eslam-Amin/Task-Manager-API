f5325e69fc6bf5605b9abdce97550856a26dc55d
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-27 17:34:32 +0200

    Included appRoutes

--- startup/app.js
+++ startup/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
 
+const appRoutes = require("./routes.js");
 const globalError = require("../middlewares/error.middleware");
 
 module.exports = (app) => {
@@ -13,6 +14,9 @@ module.exports = (app) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.json({ limit: "25kb" }));
+
+  app.use("/api/v1", appRoutes);
+
 
   // Global Error Handler
   app.use(globalError);

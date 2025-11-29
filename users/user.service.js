5f62b5d66222e0d0826cb1cc09a975a05d4cfc9c
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-29 02:39:45 +0200

    file formatting

--- users/user.service.js
+++ users/user.service.js
@@ -1,9 +1,9 @@
 /**
  * User Service
- * 
+ *
  * This module contains the business logic for user-related operations.
  * It handles user creation, retrieval, updating, and deletion.
- * 
+ *
  * @module users/user.service
  */
 
@@ -12,7 +12,7 @@ const ApiError = require("../utils/ApiError");
 
 /**
  * User Service Class
- * 
+ *
  * Provides methods for managing user data in the database.
  * All database operations are performed through Mongoose models.
  */
@@ -23,9 +23,9 @@ class UserService {
 
   /**
    * Create a New User
-   * 
+   *
    * Creates a new user in the database with hashed password.
-   * 
+   *
    * @param {Object} data - User data
    * @param {string} data.password - Plain text password (will be hashed)
    * @param {Object} ...data - Other user fields (firstName, lastName, email, etc.)
@@ -35,19 +35,19 @@ class UserService {
   async createOne(data) {
     // Hash the password before storing
     const hashedPassword = await Hash.hashKey(data.password);
-    
+
     // Create new user instance with hashed password
     const user = new this.UserModel({ ...data, password: hashedPassword });
-    
+
     // Save user to database
     return await user.save();
   }
 
   /**
    * Get All Users
-   * 
+   *
    * Retrieves all users from the database.
-   * 
+   *
    * @returns {Promise<Array>} - Array of user objects
    */
   async getAll() {
@@ -56,29 +56,29 @@ class UserService {
 
   /**
    * Get User by ID
-   * 
+   *
    * Retrieves a single user by their MongoDB ObjectId.
-   * 
+   *
    * @param {string} id - User's MongoDB ObjectId
    * @returns {Promise<Object>} - User object
    * @throws {ApiError} - If user is not found (404)
    */
   async getOneById(id) {
     const user = await this.UserModel.findById(id);
-    
+
     if (!user) {
       throw ApiError.notFound(`User is not found`);
     }
-    
+
     return user;
   }
 
   /**
    * Get User by Email
-   * 
+   *
    * Retrieves a user by their email address.
    * Uses case-insensitive collation for email matching.
-   * 
+   *
    * @param {string} email - User's email address
    * @returns {Promise<Object|null>} - User object if found, null otherwise
    */
@@ -88,15 +88,15 @@ class UserService {
       locale: "en",
       strength: 2 // Case-insensitive comparison
     });
-    
+
     return user;
   }
 
   /**
    * Update User
-   * 
+   *
    * Updates user information in the database.
-   * 
+   *
    * @param {string} id - User's MongoDB ObjectId
    * @param {Object} data - Updated user data
    * @returns {Promise<Object>} - Updated user object
@@ -107,30 +107,30 @@ class UserService {
     const user = await this.UserModel.findByIdAndUpdate(id, data, {
       new: true // Return updated document instead of original
     });
-    
+
     if (!user) {
       throw ApiError.notFound(`User is not found`);
     }
-    
+
     return user;
   }
 
   /**
    * Delete User
-   * 
+   *
    * Deletes a user from the database.
-   * 
+   *
    * @param {string} id - User's MongoDB ObjectId
    * @returns {Promise<Object>} - Deleted user object
    * @throws {ApiError} - If user is not found (404)
    */
   async deleteOne(id) {
     const user = await this.UserModel.findByIdAndDelete(id);
-    
+
     if (!user) {
       throw ApiError.notFound(`User is not found`);
     }
-    
+
     return user;
   }
 }
